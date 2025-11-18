805361935682c2528c3ffdb4f83cf14d2c30e140
Charlie <112635823+charliepain@users.noreply.github.com>
2025-11-17 22:03:33 -0500

    Comment more tests to check pitest actions on core

--- core/src/test/java/com/graphhopper/util/ArrayUtilExtraTest.java
+++ core/src/test/java/com/graphhopper/util/ArrayUtilExtraTest.java
@@ -8,16 +8,16 @@ import static org.junit.jupiter.api.Assertions.*;
 public class ArrayUtilExtraTest {
     @Test
     public void testRemoveConsecutiveDuplicatesDupesOnlyAfterEnd() {
-        int[] dupesOnlyAfterEnd = {0, 1, 2, 3, 4, 7, 7, 7};
-        assertEquals(5, ArrayUtil.removeConsecutiveDuplicates(dupesOnlyAfterEnd, 5));
-        assertEquals(IntArrayList.from(0, 1, 2, 3, 4, 7, 7, 7), IntArrayList.from(dupesOnlyAfterEnd));
+//        int[] dupesOnlyAfterEnd = {0, 1, 2, 3, 4, 7, 7, 7};
+//        assertEquals(5, ArrayUtil.removeConsecutiveDuplicates(dupesOnlyAfterEnd, 5));
+//        assertEquals(IntArrayList.from(0, 1, 2, 3, 4, 7, 7, 7), IntArrayList.from(dupesOnlyAfterEnd));
     }
 
     @Test
     public void testTransformMapTooSmall() {
-        IntArrayList arrWithLargeNum = IntArrayList.from(1, 2, 3, 4, 9, 3);
-        IntArrayList smallMap = IntArrayList.from(10, 11, 12, 13, 14);
-        assertThrows(Error.class, () -> ArrayUtil.transform(arrWithLargeNum, smallMap));
+//        IntArrayList arrWithLargeNum = IntArrayList.from(1, 2, 3, 4, 9, 3);
+//        IntArrayList smallMap = IntArrayList.from(10, 11, 12, 13, 14);
+//        assertThrows(Error.class, () -> ArrayUtil.transform(arrWithLargeNum, smallMap));
     }
 
 //    @Test
