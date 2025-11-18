180b48d528ce7732dc347de1ea886dd90022f077
Charlie <112635823+charliepain@users.noreply.github.com>
2025-11-17 22:13:26 -0500

    Comment all core extra tests to check pitest actions on core

--- core/src/test/java/com/graphhopper/util/DistanceCalcEuclideanExtraTest.java
+++ core/src/test/java/com/graphhopper/util/DistanceCalcEuclideanExtraTest.java
@@ -9,19 +9,19 @@ import static org.junit.jupiter.api.Assertions.*;
 public class DistanceCalcEuclideanExtraTest {
     @Test
     public void testCalcDist3DNormalXYZ() {
-        DistanceCalcEuclidean distCalc = new DistanceCalcEuclidean();
-        assertEquals(12.328828, distCalc.calcDist3D(
-                1, -3, 2,
-                5, 7, 8
-        ), 1e-6);
+//        DistanceCalcEuclidean distCalc = new DistanceCalcEuclidean();
+//        assertEquals(12.328828, distCalc.calcDist3D(
+//                1, -3, 2,
+//                5, 7, 8
+//        ), 1e-6);
     }
 
-    @Test
-    public void testIntermediatePointNormalInputs() {
-        DistanceCalcEuclidean distCalc = new DistanceCalcEuclidean();
-        GHPoint ghPoint = distCalc.intermediatePoint(0.7, 1, 3, 11, 8);
-        assertEquals(8, ghPoint.lat);
-        assertEquals(6.5, ghPoint.lon);
-    }
+//    @Test
+//    public void testIntermediatePointNormalInputs() {
+//        DistanceCalcEuclidean distCalc = new DistanceCalcEuclidean();
+//        GHPoint ghPoint = distCalc.intermediatePoint(0.7, 1, 3, 11, 8);
+//        assertEquals(8, ghPoint.lat);
+//        assertEquals(6.5, ghPoint.lon);
+//    }
 
 }
